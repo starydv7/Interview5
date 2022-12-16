e3aed38481bb50793e21de356c66bdbaf70f379c
starydv7 <pawanstar72926@gmail.com>
2022-12-17 01:27:28 +0530

    res

--- output.js
+++ output.js
@@ -1,3 +1,11 @@
 var res = 4 + "3" + 2 + 6;
 console.log(res);
 console.log(typeof (res));
+
+
+function AddNumber() {
+    for (var i = 0; i < 5; i++){
+        console.log(i);
+    }
+}
+AddNumber();
